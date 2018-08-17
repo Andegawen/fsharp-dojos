aa2850dce96b70d044fbe9cb4372b135e105f221
Krzysztof Kroczak <krzysztof.kroczak@red-gate.com>
2018-08-17 16:01:41 +0100

    Josh refactorings

--- 17_08/Minesweeper/Program.fs
+++ 17_08/Minesweeper/Program.fs
@@ -5,11 +5,7 @@ type Cell = Mine|Hint of int
 let mines = [{X=25;Y=25};{X=26;Y=26}]
 
 let getBlowArea mine =
-    seq {
-        for x = -1 to 1 do 
-            for y = -1 to 1 do
-                yield {X = mine.X + x; Y = mine.Y + y}
-    }
+    Seq.collect (fun x-> Seq.map (fun y -> {X = x;Y=y}) { mine.X-1.. mine.X+1 })  { mine.Y-1.. mine.Y+1 }
 
 let transformToHints mines =
     Seq.collect (fun m -> getBlowArea m) mines
@@ -24,8 +20,8 @@ let printCoord coord combined =
 
 let printBoard mines maxX maxY =
     let combined = Map.ofSeq (transformToHints mines) 
-    for x = 0 to maxX - 1 do
-        for y = 0 to maxY - 1 do
+    for x in { 0..maxX-1 } do
+        for y in { 0..maxY-1 } do
             printf "%s" (printCoord {X = x;Y = y} combined)
         printfn ""
 
